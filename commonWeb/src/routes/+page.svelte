ea2c15953a0f8a9475722346104470bbbb114fad
tizu69 <60812901+tizu69@users.noreply.github.com>
2025-07-13 12:39:53 +0200

    someone in hexcord asked for this idk

--- commonWeb/src/routes/+page.svelte
+++ commonWeb/src/routes/+page.svelte
@@ -5,18 +5,19 @@
 <h1>4x: Melcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-<!-- style="background: rgb({colors.get("WOOD", 0).join(",")});" -->
 <div class="bgc-root m-4 w-fit">
 	<div class="flex justify-between pb-3">
 		<p class="text-gray">Chess Chest</p>
-		<div class="size-7" style="background: rgb({colors.get('WOOD', 0).join(',')});"></div> 
+		<div class="size-7" style="background: rgb({colors.get('DIAMOND', 0).join(',')});"></div>
 	</div>
-	<div class="grid grid-cols-8 w-fit mx-auto">
-		{#each { length: 8*8 } as _, i}
-			<div class={[
-				"size-16",
-				(i+Math.floor(i/8)) % 2 == 0 ? "bgc-inset" : "bgc-insetlong"
-			]}></div>
+	<div class="mx-auto grid w-fit grid-cols-8">
+		{#each { length: 8 * 8 } as _, i}
+			<div
+				class={[
+					'size-16',
+					(i + Math.floor(i / 8)) % 2 == 0 ? 'bgc-inset' : 'bgc-insetlong'
+				]}
+			></div>
 		{/each}
 	</div>
 
@@ -25,7 +26,41 @@
 		<input type="text" class="input bgc-inset text-shadow w-64 px-2" placeholder="Search..." />
 	</div>
 	<div class="grid grid-cols-9">
-		{#each { length: 9*3 } as _}
+		{#each { length: 9 * 3 } as _}
+			<div class="bgc-inset size-16"></div>
+		{/each}
+	</div>
+	<div class="grid grid-cols-9 pt-3">
+		{#each { length: 9 } as _}
+			<div class="bgc-inset size-16"></div>
+		{/each}
+	</div>
+</div>
+
+<div class="bgc-root m-4 w-fit">
+	<div class="flex justify-between pb-3">
+		<p class="text-gray">Donut Chest</p>
+		<div class="size-7" style="background: rgb({colors.get('WOOD', 0).join(',')});"></div>
+	</div>
+	<div class="mx-auto grid w-fit grid-cols-9">
+		{#each { length: 9 * 9 } as _, i}
+			{@const col = Math.floor(i / 9)}
+			{@const row = i % 9}
+			<div
+				class={[
+					'bgc-inset size-16',
+					col > 1 && col < 7 && row > 1 && row < 7 && 'opacity-0'
+				]}
+			></div>
+		{/each}
+	</div>
+
+	<div class="flex items-center justify-between py-1">
+		<p class="text-gray">Inventory</p>
+		<input type="text" class="input bgc-inset text-shadow w-64 px-2" placeholder="Search..." />
+	</div>
+	<div class="grid grid-cols-9">
+		{#each { length: 9 * 3 } as _}
 			<div class="bgc-inset size-16"></div>
 		{/each}
 	</div>
