410cb4a9aa07559f8479bfff6a863ddec01743b8
tizu69 <60812901+tizu69@users.noreply.github.com>
2025-07-19 20:46:35 +0200

    add meta tags for discord embed

--- commonWeb/src/routes/+layout.svelte
+++ commonWeb/src/routes/+layout.svelte
@@ -5,10 +5,3 @@
 </script>
 
 {@render children()}
-
-<svelte:head>
-	<meta name="og:title" content="CraftMaps" />
-	<meta name="og:description" content="A Minecraft map like the real ones" />
-	<meta name="theme-color" content="#7fcc19" />
-</svelte:head>
-
